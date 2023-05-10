eb30b66bbd83dfbfa130f8aabe8f9007f92e203e
thanhhung270502 <thanhhung270502@gmail.com>
2023-05-10 17:21:48 +0700

    Update example3

--- ex2.js
+++ ex2.js
@@ -18,7 +18,7 @@ function countExplodedGems(gems, hits) {
     let cur_color = gems[0][2];
     let dp_row = [];
     let streak_str = gems[0][1],
-      streak_end = gems[0][1] - 1;
+        streak_end = gems[0][1] - 1;
     // Update the dp object
     gems.forEach((gem, index) => {
       // Jump to next row, update variable
@@ -41,19 +41,21 @@ function countExplodedGems(gems, hits) {
         }
         if (index === gems.length - 1) {
             dp_row.push([streak_str, streak_end, cur_color]);
+            dp[cur_row + 1] = dp_row;
         }
     });
-    // console.log({ dp });
+    // console.log(dp);
     let res = 0;
-    let visited = [];
+    let visited = new Set();
     function utils(cur_color, str, end, row, print = "") {
-        // print += "    ";
+        print += "    ";
         // console.log(print, "start utils", { cur_color, str, end, row });
-        if (row.toString() in visited) {
+        // console.log(print, { visited });
+        if (visited.has(row)) {
             // console.log(print, "visited row", row);
             return;
         }
-        visited.push(row.toString());
+        visited.add(row);
         if (row.toString() in dp) {
             let found = false;
             // console.log(print, { row });
@@ -80,62 +82,58 @@ function countExplodedGems(gems, hits) {
         }
     }
     for (let hit of hits) {
-        visited = [];
+        visited = new Set();
         cur_color = -1;
         let str = hit[1],
             end = hit[1];
         utils(cur_color, str, end, hit[0] + 1);
         // console.log("after hit", hit, dp);
     }
-    // console.log(res);
     return res;
   }
   
-  console.log(
+console.log(
     countExplodedGems(
-        [
-            [2, 6, 1],
-            [0, 6, 1],
-            [4, 6, 1],
-            [4, 10, 2],
-            [5, 8, 2],
-            [2, 11, 2],
-            [0, 7, 1],
-            [2, 5, 1],
-            [2, 7, 1],
-            [5, 7, 1],
-            [3, 8, 2],
-            [5, 10, 2],
-            [2, 4, 1],
-            [0, 10, 2],
-            [2, 9, 2],
-            [0, 8, 2],
-            [0, 9, 2],
-            [3, 11, 2],
-            [4, 5, 1],
-            [1, 8, 2],
-            [5, 5, 1],
-            [3, 10, 2],
-            [2, 2, 1],
-            [4, 8, 2],
-            [3, 6, 1],
-            [1, 10, 2],
-            [4, 9, 2],
-            [5, 6, 1],
-            [2, 8, 2],
-            [3, 5, 1],
-            [2, 3, 1],
-            [2, 10, 2],
-            [3, 7, 1],
-            [4, 7, 1],
-            [0, 5, 1],
-        ],
-        [
-            [2, 2],
-            [2, 9],
-            [2, 2],
-        ]
-    )
-  );
-  // console.timeEnd("timing");
-  
+    [
+        [2, 6, 1],
+        [0, 6, 1],
+        [4, 6, 1],
+        [4, 10, 2],
+        [5, 8, 2],
+        [2, 11, 2],
+        [0, 7, 1],
+        [2, 5, 1],
+        [2, 7, 1],
+        [5, 7, 1],
+        [3, 8, 2],
+        [5, 10, 2],
+        [2, 4, 1],
+        [0, 10, 2],
+        [2, 9, 2],
+        [0, 8, 2],
+        [0, 9, 2],
+        [3, 11, 2],
+        [4, 5, 1],
+        [1, 8, 2],
+        [5, 5, 1],
+        [3, 10, 2],
+        [2, 2, 1],
+        [4, 8, 2],
+        [3, 6, 1],
+        [1, 10, 2],
+        [4, 9, 2],
+        [5, 6, 1],
+        [2, 8, 2],
+        [3, 5, 1],
+        [2, 3, 1],
+        [2, 10, 2],
+        [3, 7, 1],
+        [4, 7, 1],
+        [0, 5, 1],
+    ],
+    [
+        [2, 2],
+        [2, 9],
+        [2, 2],
+    ])
+);
